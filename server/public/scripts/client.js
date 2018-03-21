0c498cef57a74d8c494f93d3368bc0ebceca1745
Ally <ally@primeacademy.io>
2018-03-21 13:30:49 -0500

    Functional GET and POST

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -1,1 +1,33 @@
 console.log('JS');
+
+let app = angular.module('FoodApp', []);
+
+app.controller('FoodController', ['$http', function($http) {
+    console.log('FoodController has been loaded');
+
+    let self = this;
+    self.foodArray = [];
+
+    self.getFood = function () {
+        $http({
+            method: 'GET',
+            url: '/food'
+        }).then(function(response) {
+            console.log('GET response: ', response);
+            self.foodArray = response.data;
+        });
+    };
+
+    self.addFood = function(newFood) {
+        $http({
+            method: 'POST',
+            url: '/food',
+            data: newFood
+        }).then(function(response) {
+            console.log('POST response: ', response);
+            self.getFood();
+        });
+    };
+
+    self.getFood();
+}]);
